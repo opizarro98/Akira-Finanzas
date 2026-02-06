d261715a995d180f0733ac3cf51e9410523da73e
oscar.pizarro <oscar.pizarro@ucuenca.edu.ec>
2026-02-06 16:29:10 -0500

    FEATURE: add CORS configuration to allow requests from Angular frontend

---
+++ src/main/java/com/ec/akirafinanzas/config/CorsConfig.java
@@ -0,0 +1,26 @@
+package com.ec.akirafinanzas.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // todas las rutas
+                        .allowedOrigins(
+                                "http://localhost:4200") // origen de tu frontend Angular
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                        .allowedHeaders("*")
+                        .exposedHeaders("Authorization", "Content-Type")
+                        .allowCredentials(true) // si usas cookies / Authorization con credenciales
+                        .maxAge(3600);
+            }
+        };
+    }
+}
